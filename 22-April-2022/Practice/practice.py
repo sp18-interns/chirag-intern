45d4d81992fed48523444230d12d6fe57f589ecd
Chiragsahusparkeighteen <chirag.sahu@sparkeighteen.com>
2022-04-22 08:40:22 +0530

    Practice

--- 22-April-2022/Practice/practice.py
+++ 22-April-2022/Practice/practice.py
@@ -17,6 +17,46 @@ class Student:
         return f"The Name of the student is {self.student_name} and obtained  marks  :{self.student_marks}"
 
 
+    def set_name(self,student_name):
+        """
+
+        :param student_name:
+        :return:
+        """
+        self.name = student_name
+        assert type(student_name)==str , 'string object expected'
+
+
+    def get_name(self,student_name):
+        """
+
+        :param student_name:
+        :return:
+        """
+        return self.student_name
+
+    def set_marks(self,student_marks):
+        """
+
+        :param student_marks:
+        :return:
+        """
+        self.marks = student_marks
+        assert type(student_marks)== float , 'Float object expected'
+
+
+    def get_marks(self,student_marks):
+        """
+
+        :param student_marks:
+        :return:
+        """
+        return self.student_marks
+
+
+
+
+
 
 
 
